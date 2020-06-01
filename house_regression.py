605d07ad76442e0169c4e623fcacfaabd28f29fd
rog <rjbaladi1@gmail.com>
2020-06-01 19:41:31 +0400

    Feature engineering and imputation

--- house_regression.py
+++ house_regression.py
@@ -8,13 +8,26 @@ import matplotlib.pyplot as plt
 # Write a function to import the data
 
 test = pd.read_csv(os.path.join('dataset','test.csv'))
-
 train = pd.read_csv(os.path.join('dataset','train.csv'))
 sub = pd.read_csv(os.path.join('submission','sample_submission.csv'))
 
-# print(test.shape)
-# print(sub.head())
-#
+train.drop(columns=['Id'], axis=1, inplace=True)
+test.drop(columns=['Id'], axis=1, inplace=True)
+
+# Saving the target values in "y_train".
+y = train['SalePrice'].reset_index(drop=True)
+
+previous_train = train.copy()
+
+df = pd.concat((train, test)).reset_index(drop = True)
+
+# Dropping the target variable.
+df.drop(['SalePrice'], axis = 1, inplace = True)
+
+print('Train shape: ', train.shape)
+print('Test shape: ', test.shape)
+print('All data shape: ', df.shape)
+
 # #EDA
 # ''' 1. Correlation
 #     2. Distribution
@@ -25,12 +38,6 @@ sub = pd.read_csv(os.path.join('submission','sample_submission.csv'))
 #     7. Hyper parameter tuning
 #     '''
 
-# print(train.head())
-# print(train.shape)
-# print(train.info())
-# print(train.skew())
-
-
 # print('Initial type: ', train.MSSubClass.dtypes)
 # print('Number of unique values: ', len(train['MSSubClass'].unique()))
 # print('min: ', min(train.MSSubClass), 'max: ', max(train.MSSubClass))
@@ -38,6 +45,7 @@ sub = pd.read_csv(os.path.join('submission','sample_submission.csv'))
 
 
 def type_tool(x, data):
+    ''' function to describe a specific column in the data set'''
     n = data[x].dtypes
     m = len(data[x].unique())
     o = min(data[x])
@@ -48,81 +56,78 @@ def type_tool(x, data):
     return df
 
 
-# print(train.dtypes.value_counts())
-
-date_cols = ['YearBuilt', 'YearRemodAdd', 'GarageYrBlt', 'MoSold', 'YrSold']
-int_object = ['MSSubClass', 'OverallQual', 'OverallCond', 'GarageCars']
-
-
 def con_type(col, type, data):
+    ''' Function to converst feature type '''
     data[col] = data[col].astype(type)
     return data[col]
 
 
-con_type(date_cols, 'datetime64[ns]', train)
-con_type(int_object, 'object', train)
-
-# print(train.dtypes.value_counts())
-
-# Handling the missing data
-
 def null_per(data):
+    '''Calculates the percentage of missing features in a dataframe'''
     total = data.isnull().sum().sort_values(ascending=False)
     percent = (data.isnull().sum() / data.isnull().count()).sort_values(ascending=False)
     dtype = data.dtypes
     missing_data = pd.concat([total, percent, dtype], axis=1, keys=['Total', 'Percent', 'type'])
     return missing_data
 
+def plot_missing(df):
+    ''' Bar plot to show percentage of missing values in df dataframe and color indicates dtypes of features '''
+    sns.set_style("dark")
+    f, ax = plt.subplots(figsize=(8, 7))
+    sns.set_color_codes(palette='deep')
+    missing = null_per(df)
+    missing = missing[missing.Percent > 0]
+    missing.sort_values(by='Percent', inplace=True)
+    g = sns.barplot(x=missing.index, y='Percent', hue='type', data=missing)
+    g.set_xticklabels(g.get_xticklabels(), rotation=90)
 
-print(null_per(train))
-print('Number of missing values: ', train.isna().sum().sum())
+print('Number of missing values: ', df.isna().sum().sum())
+# plot_missing(df)
+# plt.show()
 
+convert_str = ['MSSubClass', 'YrSold', 'MoSold']
+for i in convert_str:
+    con_type(i, 'str', df)
 
-# Visualize missing values
+df['LotFrontage'] = df.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.mean()))
+df['MSZoning'] = df.groupby('MSSubClass')['MSZoning'].transform(lambda x: x.fillna(x.mode()[0]))
 
-sns.set_style("dark")
-f, ax = plt.subplots(figsize= (8,7))
-sns.set_color_codes(palette='deep')
-missing = null_per(train)
-missing = missing[missing.Percent>0]
-missing.sort_values(by='Percent', inplace=True)
-g = sns.barplot(x=missing.index, y='Percent', hue='type', data=missing)
-g.set_xticklabels(g.get_xticklabels(), rotation=90)
+mode_fill = ['Functional', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'SaleType', 'Electrical']
+none_fill = ['Alley', 'MiscFeature', 'Fence', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'MasVnrType']
+zero_fill = ['BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath', 'GarageArea', 'GarageCars', 'GarageYrBlt', 'MasVnrArea']
+drop_col = ['Utilities', 'Street', 'PoolQC']
 
+df = df.drop(drop_col, axis=1)
 
+for i in mode_fill:
+    df[i] = df[i].fillna(df[i].mode()[0])
 
-# Columns to drop with above 80 percent of missing values
+for i in none_fill:
+    df[i] = df[i].fillna('None')
 
-train.drop(columns=['PoolQC', 'MiscFeature', 'Alley', 'Fence'], axis=1, inplace=True)
-# print(null_per(train).sort_values(by=['Total','type'], ascending=[False, True]).head(50))
-print('Number of missing values: ', train.isna().sum().sum())
+for i in zero_fill:
+    df[i] = df[i].fillna(0)
 
+print('Number of missing values: ', df.isna().sum().sum())
+print('All data after imputation: ', df.shape)
 
-# Examining outliers in numerical features
-# The plot shows that we have outliers for the numerical features
+# Feature engineering more columns that can be grouped together
 
-numerical_features = ['LotFrontage', 'MasVnrArea']
+df['total_bsmt_sf'] = df['TotalBsmtSF']+df['1stFlrSF'] + df['2ndFlrSF']
+df['yrblt_remod'] = df['YearBuilt'] + df['YearRemodAdd']
+df['total_house_sf'] = df['BsmtFinSF1'] + df['BsmtFinSF2'] + df['1stFlrSF'] + df['2ndFlrSF']
+df['nb_baths'] = df['FullBath'] + (0.5 * df['HalfBath']) + df['BsmtFullBath'] + (0.5 * df['BsmtHalfBath'])
+df['porch'] = df['OpenPorchSF'] + df['3SsnPorch'] + df['EnclosedPorch'] + df['ScreenPorch'] + df['WoodDeckSF']
 
-# print(train.LotFrontage.mean(), train.LotFrontage.median())
-# print(train.MasVnrArea.mean(), train.MasVnrArea.median())
+print(df.shape)
 
-for col in numerical_features:
-    train[col].fillna(train[col].median(), inplace=True)
+df['haspool'] = df['PoolArea'].apply(lambda x: 1 if x > 0 else 0)
+df['has2ndfloor'] = df['2ndFlrSF'].apply(lambda x: 1 if x > 0 else 0)
+df['hasgarage'] = df['GarageArea'].apply(lambda x: 1 if x > 0 else 0)
+df['hasbsmt'] = df['TotalBsmtSF'].apply(lambda x: 1 if x > 0 else 0)
+df['hasfireplace'] = df['Fireplaces'].apply(lambda x: 1 if x > 0 else 0)
 
-print('Number of missing values: ', train.isna().sum().sum())
+print(df.shape)
 
-sns.set_style("dark")
-f, ax = plt.subplots(figsize= (8,7))
-sns.set_color_codes(palette='deep')
-missing = null_per(train)
-missing = missing[missing.Percent>0]
-missing.sort_values(by='Percent', inplace=True)
-g = sns.barplot(x=missing.index, y='Percent', hue='type', data=missing)
-g.set_xticklabels(g.get_xticklabels(), rotation=90)
-plt.show()
-
-# Taking a look at the date columns
-
-
-
-# Once the categories are grouped we can start analyzing and imputing the missing values
+# Create a list of columns that need to be categories
+print(df.dtypes.value_counts())
